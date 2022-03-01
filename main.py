44898b9c39e476596846e8aa8e13455b750c126e
Muhammad Jaysy Ansharulloh <jaysyanshar98@gmail.com>
2022-03-01 19:44:53 +0700

    add multiply feature

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 def add(x: float, y: float) -> float:
     return x + y
 
+def multiply(x: float, y: float) -> float:
+    return x * y
+
 x_in = input("x = ")
 y_in = input("y = ")
+opt_in = input("1. add\n2. multiply\n")
+
 try:
     x = float(x_in)
     y = float(y_in)
-    print("x + y = " + str(x + y))
+    opt = int(opt_in)
+    if opt == 1:
+        print("x + y = " + str(add(x, y)))
+    elif opt == 2:
+        print("x * y = ", str(multiply(x, y)))
 except:
     print("input should be a number")
